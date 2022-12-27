bd0fa1bcf0c55e3048034d11a83827de153e4526
Daniela Lara <laradni@icloud.com>
2022-12-27 14:27:13 -0600

    (Mod) Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Landing from "./pages/Landing"
+
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Landing/>
     </div>
   )
 }
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,13 @@
+import Header from "../components/Header";
+
 function Landing() {
-    
+    return (<div className="landing-container">
+        <div className="main">
+            <Header/>
+            <div className="pet-img"></div>
+            <div className="info"></div>
+        </div>
+    </div>)
 }
 
 export default Landing;
