f092f3c9778e6ab13b0bfa0ada36c64179bda013
Daniela Lara <laradni@icloud.com>
2022-12-27 14:25:58 -0600

    (Add) Header component file + styles

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(<header className="header">
+        <nav className="header-nav">
+            <img src="src/assets/logo.png" alt="4 vets n' pets logo" className="header-nav__logo"/>
+            <h2 className="header-nav__title">4VetsnPets</h2>
+        </nav>
+    </header>)
+}
+
+export default Header;
