5da76d77445ec487adc86accc10a0201a5291f49
Daniela Lara <laradni@icloud.com>
2023-01-09 14:07:21 -0600

    (Mod) Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header(){
     return(<header className="header">
         <nav className="header-nav">
-            <img src="src/assets/logo.png" alt="4 vets n' pets logo" className="header-nav__logo"/>
+            <img src="src/assets/img/logo.png" alt="4 vets n' pets logo" className="header-nav__logo"/>
             <h2 className="header-nav__title">4VetsnPets</h2>
         </nav>
     </header>)
