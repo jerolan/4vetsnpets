87d6d8e654bddf6adbceca592509364a0551dc61
Daniela Lara <laradni@icloud.com>
2023-01-09 14:05:37 -0600

    (Mod) Coming Soon component

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,7 +1,17 @@
+import * as FaIcons from 'react-icons/fa';
+
 function ComingSoon() {
-    return(<>
-        <h1>Coming Soon!!</h1>
-    </>);
+  return (
+    <div className="comingSoon-container">
+      <div className="sign">
+        <div className="sign-icon">
+            <FaIcons.FaTools/>
+        </div>
+        <h1>COMING SOON!</h1>
+        <img src="src/assets/img/confused-dog.png" alt="Confused pug" className='sign-img'/>
+      </div>
+    </div>
+  );
 }
 
 export default ComingSoon;
