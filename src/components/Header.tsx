cdea450b210384fb8b3c8f2a6b1814b530982ad7
Daniela Lara <laradni@icloud.com>
2023-01-10 15:12:18 -0600

    (Mod) Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 function Header(){
     return(<header className="header">
         <nav className="header-nav">
-            <img src="src/assets/img/logo.png" alt="4 vets n' pets logo" className="header-nav__logo"/>
+            <Link to="/"><img src="src/assets/img/logo.png" alt="4 vets n' pets logo" className="header-nav__logo"/></Link>
             <h2 className="header-nav__title">4VetsnPets</h2>
         </nav>
     </header>)
