f420fc2dfb10bdcc23d783bef1abe183f37a10a6
Daniela Lara <laradni@icloud.com>
2023-01-10 14:28:17 -0600

    (Mod) Landing page styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Landing/> */}
+      <Landing/>
       {/* <SignIn/> */}
-      <ComingSoon/>
+      {/* <ComingSoon/> */}
     </div>
   )
 }
