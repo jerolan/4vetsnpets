2240c79592482a0dbecb282f6207bcc0782a9074
Daniela Lara <laradni@icloud.com>
2023-01-11 17:09:54 -0600

    (Add) Home route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Landing from "../pages/Landing";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
+import Home from "../pages/Home";
 
 export const router = createBrowserRouter([
   {
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
   {
     path: "/signin",
     element: <SignIn />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
   },
 ]);
