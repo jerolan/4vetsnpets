e78f437b2fb2793421d3122e4886fea0f2cc4a01
Daniela Lara <laradni@icloud.com>
2023-01-11 14:49:35 -0600

    (Add) Firebase config file

---
+++ src/services/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBGrD-QnejJbTLImMp5qpufTYk803MOqQo",
+    authDomain: "vetsnpets-52c1f.firebaseapp.com",
+    projectId: "vetsnpets-52c1f",
+    storageBucket: "vetsnpets-52c1f.appspot.com",
+    messagingSenderId: "800037039816",
+    appId: "1:800037039816:web:07111d37cc6e4c98377ffd",
+  },
+};
