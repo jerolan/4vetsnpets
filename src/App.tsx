362636f579c9b6b3c9273df936d88e2b98c12a4d
Daniela Lara <laradni@icloud.com>
2023-01-11 17:35:48 -0600

    (Mod) firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import {router} from './router';
 import { RouterProvider } from "react-router-dom";
-import { initializeApp } from "firebase/app";
-import { config } from './services/config';
-
-initializeApp(config.firebaseConfig);
 
 function App (){
     return(<main className='app'>
--- src/services/config.ts
+++ src/services/config.ts
@@ -1,10 +1,17 @@
-export const config = {
-  firebaseConfig: {
-    apiKey: "AIzaSyBGrD-QnejJbTLImMp5qpufTYk803MOqQo",
-    authDomain: "vetsnpets-52c1f.firebaseapp.com",
-    projectId: "vetsnpets-52c1f",
-    storageBucket: "vetsnpets-52c1f.appspot.com",
-    messagingSenderId: "800037039816",
-    appId: "1:800037039816:web:07111d37cc6e4c98377ffd",
-  },
+import { initializeApp } from "firebase/app";
+
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGrD-QnejJbTLImMp5qpufTYk803MOqQo",
+  authDomain: "vetsnpets-52c1f.firebaseapp.com",
+  projectId: "vetsnpets-52c1f",
+  storageBucket: "vetsnpets-52c1f.appspot.com",
+  messagingSenderId: "800037039816",
+  appId: "1:800037039816:web:07111d37cc6e4c98377ffd"
 };
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
