859cb605f849a2c482fe1ab8e3ab4be8adb72021
Daniela Lara <laradni@icloud.com>
2023-01-11 14:49:03 -0600

    (Mod) Main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./styles/styles.scss";
-import {router} from './router';
-import { RouterProvider } from "react-router-dom";
+
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App/>
   </React.StrictMode>
 );
