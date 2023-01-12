7ca07e14ae124c143754b84b04cec41c91134380
Daniela Lara <laradni@icloud.com>
2023-01-12 13:16:32 -0600

    (Mod) firebase config

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-
+import { getAuth } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
@@ -8,10 +8,11 @@ const firebaseConfig = {
   projectId: "vetsnpets-52c1f",
   storageBucket: "vetsnpets-52c1f.appspot.com",
   messagingSenderId: "800037039816",
-  appId: "1:800037039816:web:07111d37cc6e4c98377ffd"
+  appId: "1:800037039816:web:07111d37cc6e4c98377ffd",
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
 
 export default firebaseApp;
