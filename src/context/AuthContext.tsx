885afffc5a4e2f4fed18f91307c392607d82edf2
Daniela Lara <laradni@icloud.com>
2023-01-13 14:40:14 -0600

    (Mod) Auth context file

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -11,9 +11,21 @@ const UserContext = createContext<any>({});
 
 export function AuthContextProvider(props:any) {
 
+  const [user, setUser] = useState({});
+
     function createUser(email:string, password: string, rol: string) {
         return createUserWithEmailAndPassword(auth, email, password);
     }
+
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (currentUser:any) => {
+        console.log(currentUser);
+        setUser(currentUser);
+      });
+      return () => {
+        unsubscribe();
+      };
+    }, []);
 
   return (
     <>
